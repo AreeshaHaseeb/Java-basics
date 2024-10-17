4a040d1e8100d3874ed2c11b5c880c8e51c93d4f
Areesha Haseeb Khan Suontausta <32227763+AreeshaHaseeb@users.noreply.github.com>
2024-10-17 02:25:57 +0200

    finished it since it was small

---
+++ index.js
@@ -0,0 +1,16 @@
+console.log("Hello world!");
+
+let firstName = "Areesha";
+let age = 28;
+let favouriteColor = "Black";
+
+console.log(`My name is ${firstName}, my age is ${age} and my favourite color is ${favouriteColor}`);
+
+let myAge = 28;
+let mentalAge = 24;
+
+mentalAge = 24 - 1
+myAge = 28 / 2;
+
+console.log(myAge)
+console.log(myAge + mentalAge)
